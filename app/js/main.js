9476c9133f8ce555c488705940d983ab038b9ef8
home user Stepan <home stefan.irk96@mail.ru>
2021-12-12 21:32:56 +0800

    add section excursion and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -12,4 +12,32 @@
          $('.menu__btn').removeClass('active');
        }
      }));
+
+
+     $('.excursion__slider-inner').slick({
+       prevArrow: '<button class="slick-arrow slick-prev"><div class="hand"><img src="images/icons/slider-left.png" alt=""></div></button>',
+       nextArrow: '<button class="slick-arrow slick-next"><div class="hand"><img src="images/icons/slider-right.png" alt=""></div></button>',
+       infinite: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       responsive: [{
+           breakpoint: 1350,
+           settings: {
+             slidesToShow: 2,
+           }
+         },
+         {
+           breakpoint: 975,
+           settings: {
+             slidesToShow: 1,
+           }
+         },
+         {
+           breakpoint: 550,
+           settings: {
+             slidesToShow: 1,
+           },
+         },
+       ]
+     });
    })
